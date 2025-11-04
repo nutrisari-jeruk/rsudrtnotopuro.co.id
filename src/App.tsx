915c2e181558c537451c39c746b9c0deb7370eb5
Arifin Nor <arifinnor03@gmail.com>
2025-11-04 11:54:24 +0700

    Refactor project to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className="flex items-center justify-between p-4">
+        <h1 className="text-xl font-semibold text-gray-800 dark:text-gray-100">RSUD R.T. NOTOPURO SIDOARJO</h1>
+      </header>
+      <main></main>
+    </>
+  )
+}
+
+export default App
